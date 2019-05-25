b48ae2b272554f8e449adadce555104305c926f2
Charles Alleman <charles@alleman.com.au>
2019-05-25 18:36:51 +1000

    Week 8 Workbook
    Make sure to change the database settings in ./database/db.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
-var express = require('express');
+var express = require("express");
+var mysql = require("mysql");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/api", function(req, res, next) {
+  res.render("index", { title: "The World Database API" });
+});
+
+router.get("/api/city", function(req, res, next) {
+  let query = "SELECT name, district FROM ??";
+  let table = ["city"];
+
+  query = mysql.format(query, table);
+  req.db.query(query, function(err, rows) {
+    if (err) {
+      res.json({ Error: true, Message: err });
+    } else {
+      res.json({ Error: false, Message: "Success", City: rows });
+    }
+  });
+});
+
+router.get("/api/city/:CountryCode", function(req, res) {
+  let query = "SELECT * FROM ?? WHERE ??=?";
+  let table = ["city", "CountryCode", req.params.CountryCode];
+  query = mysql.format(query, table);
+
+  req.db.query(query, function(err, rows) {
+    if (err) {
+      res.json({ Error: true, Message: err });
+    } else {
+      res.json({ Error: false, Message: "Success", City: rows });
+    }
+  });
 });
 
 module.exports = router;
